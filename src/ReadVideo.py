bd09c7a41286e58199bdf1c28a091e81474e4db9
Chris <christophercalloway216@gmail.com>
2020-05-27 09:34:51 -0700

    added basic feature detection

--- src/ReadVideo.py
+++ src/ReadVideo.py
@@ -4,6 +4,18 @@ from ExtractFeatures import FeatExtract
 cap = cv2.VideoCapture('../Videos/DSI_test_video_1.mov')
 
 
+face_cascade = cv2.CascadeClassifier()
+eyes_cascade = cv2.CascadeClassifier()
+
+# Load the cascades classifiers
+if not face_cascade.load(cv2.samples.findFile('../haarcascades/haarcascade_frontalface_alt.xml')):
+    print('--(!)Error loading face cascade')
+    exit(0)
+if not eyes_cascade.load(cv2.samples.findFile('../haarcascades/haarcascade_eye_tree_eyeglasses.xml')):
+    print('--(!)Error loading eyes cascade')
+    exit(0)
+
+
 # Check if camera opened successfully
 if (cap.isOpened() == False):
     print("Error opening video stream or file")
@@ -13,7 +25,7 @@ while(cap.isOpened()):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    feat = FeatExtract(frame)
+    feat = FeatExtract(frame, face_cascade, eyes_cascade)
     if ret == True:
 
         # Display the resulting frame
