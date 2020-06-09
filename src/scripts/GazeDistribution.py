59c5a8341961dc58b9ea197338ada2d7ae7025bf
Chris <christophercalloway216@gmail.com>
2020-06-09 13:41:09 -0700

    added more file reading capacity

--- src/scripts/GazeDistribution.py
+++ src/scripts/GazeDistribution.py
@@ -4,10 +4,11 @@ import numpy as np
 from scipy import stats
 import seaborn as sns
 import csv
+import glob
 import os
 import subprocess
 
-
+files = []
 gaze_angle_x = []
 gaze_angle_y = []
 confidence_threshold = 0.98
@@ -42,28 +43,37 @@ def read_csv_file(filename):
             counter = counter + 1
 
 
+def plot_kernels():
+
+    data = {
+        "x": gaze_angle_x,
+        "y": gaze_angle_y
+    }
+
+    g = sns.JointGrid(x="x", y="y", data=data, space=0)
+    g = g.plot_joint(sns.kdeplot, cmap="Blues_d")
+    g = g.plot_marginals(sns.kdeplot, shade=True)
+    plt.show()
 
 
 
+for file in glob.glob("../../data/Media/*.mov"):
+    video = os.path.splitext(os.path.basename(file))
+    print(video[0])
+    files.append(video[0])
 
-os.chdir("../../OpenFacePrecompiledBinaries/bin")
-subprocess.call(['./FeatureExtraction', '-f', '../../data/Media/demo_4.mov', '-gaze', '-out_dir', '../../data/gaze_outputs'])
-read_csv_file('../../data/gaze_outputs/demo_4.csv')
+for name in files:
+    file_name = name
+    os.chdir("../../OpenFacePrecompiledBinaries/bin")
+    subprocess.call(['./FeatureExtraction', '-f', '../../data/Media/' + file_name + '.mov', '-gaze', '-out_dir', '../../data/gaze_outputs'])
+    read_csv_file('../../data/gaze_outputs/' + file_name + '.csv')
+    plot_kernels()
+
+
 # sns.set(color_codes=True)
 # sns.distplot(gaze_angle_x, axlabel="Gaze Angle Radians", label="x",  kde_kws={"color": "b", "lw": 1, "label": "x-KDE"})
 # sns.distplot(gaze_angle_y, label="y",kde_kws={"color": "orange", "lw": 1, "label": "y-KDE"} )
 # plt.show()
 
-
-data = {
-    "x": gaze_angle_x,
-    "y": gaze_angle_y
-}
-
-g = sns.JointGrid(x="x", y="y", data=data, space=0)
-g = g.plot_joint(sns.kdeplot, cmap="Blues_d")
-g = g.plot_marginals(sns.kdeplot, shade=True)
-
-plt.show()
 
 print("hello world")
