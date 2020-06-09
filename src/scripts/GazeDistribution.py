abd053cecd9777417d56fe440a17ba1eb1306959
Chris <christophercalloway216@gmail.com>
2020-06-09 11:21:34 -0700

    added data visualization for eye gaze

--- src/scripts/GazeDistribution.py
+++ src/scripts/GazeDistribution.py
@@ -1,15 +1,17 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
+import seaborn as sns
 import csv
 
 
-gaze_angle_left = []
-gaze_angle_right = []
+gaze_angle_x = []
+gaze_angle_y = []
 confidence_threshold = 0.98
 
 def read_csv_file():
-    with open('../../data/gaze_outputs/up_down_test.csv', newline='') as f:
+    with open('../../data/gaze_outputs/zoom_demo_1.csv', newline='') as f:
         reader = csv.reader(f)
         counter = 0
         for row in reader:
@@ -27,11 +29,11 @@ def read_csv_file():
 
                     # Gaze Angle X is in column  11
                     if row_element == 11:
-                        gaze_angle_left.append(i)
+                        gaze_angle_x.append(i)
                         
                     # Gaze Angle Y is in column 12
                     if row_element == 12:
-                        gaze_angle_right.append(i)
+                        gaze_angle_y.append(i)
                     
                     row_element = row_element + 1
 
@@ -41,15 +43,17 @@ def read_csv_file():
 
 
 
+
 read_csv_file()
 
-plt.rcParams.update({'figure.figsize':(7,5), 'figure.dpi':100})
 
-# Plot Histogram on x
-x = np.random.normal(size = 1000)
-plt.hist(x, bins=50)
-plt.gca().set(title='Frequency Histogram', ylabel='Frequency')
+x = np.random.normal(size=100)
+
+sns.set(color_codes=True)
+sns.distplot(gaze_angle_x, axlabel="Gaze Angle Radians", label="x",  kde_kws={"color": "b", "lw": 1, "label": "x-KDE"})
+sns.distplot(gaze_angle_y, label="y",kde_kws={"color": "orange", "lw": 1, "label": "y-KDE"} )
 plt.show()
+
 
 
 
