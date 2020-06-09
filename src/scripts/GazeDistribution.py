8b1438d19dfec9b3a2a7758ffbe1f953b40ee271
Chris <christophercalloway216@gmail.com>
2020-06-09 13:11:28 -0700

    Added cleaner pipeline for reading videos

--- src/scripts/GazeDistribution.py
+++ src/scripts/GazeDistribution.py
@@ -4,14 +4,16 @@ import numpy as np
 from scipy import stats
 import seaborn as sns
 import csv
+import os
+import subprocess
 
 
 gaze_angle_x = []
 gaze_angle_y = []
 confidence_threshold = 0.98
 
-def read_csv_file():
-    with open('../../data/gaze_outputs/zoom_demo_1.csv', newline='') as f:
+def read_csv_file(filename):
+    with open(filename, newline='') as f:
         reader = csv.reader(f)
         counter = 0
         for row in reader:
@@ -44,18 +46,24 @@ def read_csv_file():
 
 
 
-read_csv_file()
+os.chdir("../../OpenFacePrecompiledBinaries/bin")
+subprocess.call(['./FeatureExtraction', '-f', '../../data/Media/demo_4.mov', '-gaze', '-out_dir', '../../data/gaze_outputs'])
+read_csv_file('../../data/gaze_outputs/demo_4.csv')
+# sns.set(color_codes=True)
+# sns.distplot(gaze_angle_x, axlabel="Gaze Angle Radians", label="x",  kde_kws={"color": "b", "lw": 1, "label": "x-KDE"})
+# sns.distplot(gaze_angle_y, label="y",kde_kws={"color": "orange", "lw": 1, "label": "y-KDE"} )
+# plt.show()
 
 
-x = np.random.normal(size=100)
+data = {
+    "x": gaze_angle_x,
+    "y": gaze_angle_y
+}
 
-sns.set(color_codes=True)
-sns.distplot(gaze_angle_x, axlabel="Gaze Angle Radians", label="x",  kde_kws={"color": "b", "lw": 1, "label": "x-KDE"})
-sns.distplot(gaze_angle_y, label="y",kde_kws={"color": "orange", "lw": 1, "label": "y-KDE"} )
+g = sns.JointGrid(x="x", y="y", data=data, space=0)
+g = g.plot_joint(sns.kdeplot, cmap="Blues_d")
+g = g.plot_marginals(sns.kdeplot, shade=True)
+
 plt.show()
-
-
-
-
 
 print("hello world")
